999e7d87754371b706c969f9bd89a40469793a1a
Kunal Jain <96196312+Kuna1jain@users.noreply.github.com>
2023-01-26 16:44:33 +0530

    Add files via upload

---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+const movie1 = {
+//     "Title": "Spiderman the Verse",
+//     "Year": "2019–",
+//     "imdbID": "tt12122034",
+//     "Type": "series",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BNjA2NmZhOGEtZTQ5OS00MDI0LTg4N2UtYTRmOTllM2I2NDlhXkEyXkFqcGdeQXVyNTU4OTE5Nzc@._V1_SX300.jpg"
+// }
